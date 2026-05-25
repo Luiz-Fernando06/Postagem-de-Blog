bec614efbc867ee82d21725282a310fc39cd7024
Luiz Fernando <e.luizfernando2006@gmail.com>
2026-05-25 18:04:59 -0300

    morango

--- run_blogsql.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Program Files\MySQL\MySQL Server 8.0\bin"
-mysql.exe -u root -proot -h 127.0.0.1 -P 3306 < "C:\Users\Cristian\OneDrive\Imagens\Postagem-de-Blog-master\Postagem-de-Blog-master\bloguinho.sql"
-
--- run_blogsql2.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Program Files\MySQL\MySQL Server 8.0\bin"
-mysql.exe --user=root --password=Tire2002@ --host=127.0.0.1 --port=3306 < "C:\Users\Cristian\OneDrive\Imagens\Postagem-de-Blog-master\Postagem-de-Blog-master\bloguinho.sql"
